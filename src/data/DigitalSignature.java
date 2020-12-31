f550cae265db8b131f6eb42b2d09fcd070e60d30
rever1300 <rever1300@gmail.com>
2020-12-31 08:52:03 +0100

    Fet més o menys DigitalSignature

--- src/data/DigitalSignature.java
+++ src/data/DigitalSignature.java
@@ -1,8 +1,41 @@
 package data;
 
+import java.util.Arrays;
+
 final public class DigitalSignature {
-    private final Byte[] medicalSignature = new Byte[];
+    private final Byte[] medicalSignature;
 
+    public DigitalSignature(int mida) { //---INTRODUIM MIDA AL BYTE[]---//
+        medicalSignature = new Byte[mida];
+    }
+    public DigitalSignature(){ //---PER DEFECTE -->20---//
+        medicalSignature = new Byte[20];
+    }
 
+    public String getDigitalSignature() {
+        return Arrays.toString(medicalSignature);
+    }
 
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(o == null || getClass()!= o.getClass()) return false;
+        DigitalSignature ds = (DigitalSignature) o;
+        return Arrays.equals(medicalSignature, ds.medicalSignature);
+    }
+
+    @Override
+    public int hashCode(){
+        return Arrays.hashCode(medicalSignature);
+    }
+
+    @Override
+    public String toString(){
+        String MSign="{ ";
+        for (Byte aByte : medicalSignature) {
+            MSign = MSign.concat(aByte + ", ");
+        }
+        MSign += " }";
+        return MSign;
+    }
 }
