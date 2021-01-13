b7b063b5000d836936bba269ac9a0e661601810f
anasanchez28 <asanchezplumed@gmail.com>
2021-01-13 16:44:13 +0100

    afegir test

--- test/medicalconsultationTest/MedicalPrescriptionLineTest.java
+++ test/medicalconsultationTest/MedicalPrescriptionLineTest.java
@@ -1,9 +1,25 @@
 package medicalconsultationTest;
 
-import medicalconsultation.MedicalPrescriptionLine;
+import Exceptions.dataE.ProductIDException;
+import data.ProductID;
+import medicalconsultation.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class MedicalPrescriptionLineTest {
     private MedicalPrescriptionLine medicalPrescriptionLine;
 
-
+    @BeforeEach
+    public void initialize() throws ProductIDException {
+        String numCode = "123456789102";
+        ProductID pID = new ProductID(numCode);
+        dayMoment DayMoment = dayMoment.DURINGMEALS;
+        float duration = 24f;
+        String instructions = "Maxim 5 pastilles per dia";
+        float dose = 1f;
+        float freq = 7f;
+        FqUnit freqUnit = FqUnit.HOUR;
+        TakingGuideline tGLine = new TakingGuideline(DayMoment, duration, instructions, dose, freq, freqUnit);
+        medicalPrescriptionLine = new MedicalPrescriptionLine(pID, tGLine);
+    }
 }
